e05880133fc36b8f0a53268fcd9bf44c37cb4796
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2020-06-05 21:04:52 +0500

    Add ShareSheet
    ORIGINAL: SOURCE: https://github.com/AlanQuatermain/SwiftUIShareSheetDemo/blob/master/ShareSheetDemo/ShareSheet.swift
    CREDIT: Copyright © 2019 Jim Dovey
    Permission pending

---
+++ Sources/OctopusUI/Controls/ShareSheet.swift
@@ -0,0 +1,56 @@
+//
+//  ShareSheet.swift
+//  OctopusUI
+//  https://github.com/InvadingOctopus/octopusui
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2020/05/23.
+//  ORIGINAL: SOURCE: https://github.com/AlanQuatermain/SwiftUIShareSheetDemo/blob/master/ShareSheetDemo/ShareSheet.swift
+//  ORIGINAL: CREDIT: Copyright © 2019 Jim Dovey. All rights reserved. https://github.com/AlanQuatermain
+//  UPDATES:  2020 Invading Octopus.
+//
+
+// TODO: macOS version
+
+import SwiftUI
+
+/// Presents a sharing sheet.
+@available(iOS 13.0, *)
+public struct ShareSheet: UIViewControllerRepresentable {
+    
+    public typealias Callback = (
+        _ activityType:  UIActivity.ActivityType?,
+        _ completed:     Bool,
+        _ returnedItems: [Any]?,
+        _ error:         Error?) -> Void
+
+    let activityItems:          [Any]
+    let applicationActivities:  [UIActivity]?
+    let excludedActivityTypes:  [UIActivity.ActivityType]?
+    let callback:               Callback?
+
+    public init(activityItems:  [Any],
+                applicationActivities: [UIActivity]?              = nil,
+                excludedActivityTypes: [UIActivity.ActivityType]? = nil,
+                callback:              ShareSheet.Callback?       = nil)
+    {
+        self.activityItems          = activityItems
+        self.applicationActivities  = applicationActivities
+        self.excludedActivityTypes  = excludedActivityTypes
+        self.callback               = callback
+    }
+    
+    public func makeUIViewController(context: Context) -> UIActivityViewController {
+        
+        let controller = UIActivityViewController(
+            activityItems:          activityItems,
+            applicationActivities:  applicationActivities)
+        
+        controller.excludedActivityTypes        = excludedActivityTypes
+        controller.completionWithItemsHandler   = callback
+        return controller
+    }
+
+    public func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+        // Not needed
+    }
+}
