9b4bb958d2b64c541dbe934e0e36de6e8ef859fa
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2023-07-06 11:30:16 +0400

    Update package to latest Swift and OS versions

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 // https://github.com/InvadingOctopus/octopusui
@@ -8,9 +8,9 @@ import PackageDescription
 let package = Package(
     name: "OctopusUI",
     platforms: [
-        .iOS(.v14),
-        .macOS(.v10_16),
-        .tvOS(.v14)
+        .iOS(.v17),
+        .macOS(.v14),
+        .tvOS(.v17)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
