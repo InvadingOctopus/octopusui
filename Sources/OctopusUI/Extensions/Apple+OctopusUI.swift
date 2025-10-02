51fc4ab4ba25f6e66a17cd12ddcac26bfcca4de4
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-10-02 11:05:37 +0500

    Add Date extensions

--- Sources/OctopusUI/Extensions/Apple+OctopusUI.swift
+++ Sources/OctopusUI/Extensions/Apple+OctopusUI.swift
@@ -10,6 +10,32 @@
 import Foundation
 import SwiftUI
 
+
+public extension Date {
+    // TODO: Move to OctopusCore
+    
+    var oneHourAgo: Date {
+        self.addingTimeInterval(-3600) // (60 * 60)
+    }
+
+    var oneDayAgo: Date {
+        self.addingTimeInterval(-86400) // (60 * 60 * 24)
+    }
+
+    var oneWeekAgo: Date {
+        self.addingTimeInterval(-604800) // (60 * 60 * 24 * 7)
+    }
+
+    var isInLastHour: Bool {
+        self.timeIntervalSinceNow >= -3600 // (60 * 60)
+    }
+
+    var isInToday: Bool {
+        self.timeIntervalSinceNow >= -86400 // (60 * 60 * 24)
+    }
+}
+
+
 extension String: @retroactive View {
     
     /// This extension allows a standalone `String` to be used as a `Text` view in a SwiftUI layout.
