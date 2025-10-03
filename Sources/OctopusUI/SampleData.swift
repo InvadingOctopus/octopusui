2099b73df8e607ad1c65889f9982134443fce5a7
ShinryakuTako <11238708+ShinryakuTako@users.noreply.github.com>
2025-10-04 01:31:16 +0500

    Add SampleData

---
+++ Sources/OctopusUI/SampleData.swift
@@ -0,0 +1,17 @@
+//
+//  Color+OctopusUI.swift
+//  OctopusUI
+//  https://github.com/InvadingOctopus/octopusui
+//
+//  Created by ShinryakuTako@invadingoctopus.io on 2025/10/04.
+//  Copyright © 2025 Invading Octopus. Licensed under Apache License v2.0 (see LICENSE.txt)
+//
+
+import Foundation
+
+
+public struct SampleData {
+
+    public static let lipsum: String = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+}
